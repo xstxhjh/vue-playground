f283f8261627b5fc2ca7a18ec3f305394bbbfb34
Jia <33480698+xstxhjh@users.noreply.github.com>
2023-04-26 16:19:49 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import fs from 'fs'
-import path from 'path'
 import { defineConfig, Plugin } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
@@ -15,7 +14,6 @@ export default defineConfig({
         }
       }
     }),
-    copyVuePlugin()
   ],
   define: {
     __COMMIT__: JSON.stringify('commit'),
@@ -25,29 +23,3 @@ export default defineConfig({
     exclude: ['@vue/repl']
   }
 })
-
-function copyVuePlugin(): Plugin {
-  return {
-    name: 'copy-vue',
-    generateBundle() {
-      const copyFile = (file: string) => {
-        const filePath = path.resolve(__dirname, file)
-        const basename = path.basename(file)
-        if (!fs.existsSync(filePath)) {
-          throw new Error(
-            `${basename} not built. ` +
-              `Run "nr build vue -f esm-browser" first.`
-          )
-        }
-        this.emitFile({
-          type: 'asset',
-          fileName: basename,
-          source: fs.readFileSync(filePath, 'utf-8')
-        })
-      }
-
-      copyFile(`../vue/dist/vue.runtime.esm-browser.js`)
-      copyFile(`../server-renderer/dist/server-renderer.esm-browser.js`)
-    }
-  }
-}
